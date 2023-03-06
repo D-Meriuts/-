8a202e10ec1de7acd6c8ddd33103b8285f4cb2c7
DMeriuts <dimameriuts@gmail.com>
2023-03-06 15:25:36 +0300

    upd

--- js/functions.js
+++ js/functions.js
@@ -10,35 +10,49 @@ const functions = {
     return n
   },
 
-  // Случайное число
-  getRandomItnegralNumber: function (min, max) {
+  // Случайное  целое число
+  // и
+    // Случайное число с плавающей точкой с указанием количества цифр после запятой
+  getRandomNumber: function (min, max, AFTERCOMMA = 0) {
     if (min < 0 || max < 0) {
       return -1;
     }
-    if (max < min) {
+    if (min > max) {
       [min, max] = [max, min];
     }
-    return Math.floor(Math.random() * (max - min +1)) + min;
+    if (AFTERCOMMA === 0) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    return Number.parseFloat((Math.random() * (max - min) + min).toFixed(AFTERCOMMA));
   },
 
-  // Случайное число с плавающей точкой с указанием количества цифр после запятой
-  getRandomItnegralNumberDigits: function (min, max, digits) {
-    if (min < 0 || max < 0) {
-      return -1;
-    }
-    if (max < min) {
-      [min, max] = [max, min];
-    }
-    return (Math.floor(Math.random() * (max - min +1)) + min).toFixed(digits);
-  },
+    // getRandomNumber(1, 2, 3);
+    // console.log(getRandomNumber(1, 2, 3))
+
 
   // Случайный элемент массива
-  getRandomElementArr: function (array) {
-    return array[getRandomItnegralNumber(0, array.length - 1)];
+  getRandomElementArray: function (array) {
+    return array[getRandomNumber(0, array.length - 1)];
   },
+
+  // Перетасовать массив
+  shuffleArray: function (array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  },
+
+  // Новый массив из другого массива
+  getRandomElementsArray: function (array) {
+    return shuffleArray(array).slice(0, getRandomNumber(1, array.length));
+  },
+
 }
 
 export const addZeros = functions.addZeros;
-export const getRandomItnegralNumber = functions.getRandomItnegralNumber;
-export const getRandomItnegralNumberDigits = functions.getRandomItnegralNumberDigits;
-export const getRandomElementArr = functions.getRandomElementArr;
+export const getRandomNumber = functions.getRandomNumber;
+export const getRandomElementArray = functions.getRandomElementArray;
+export const shuffleArray = functions.shuffleArray;
+export const getRandomElementsArray = functions.getRandomElementsArray;
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import createAdvertisementList from './data.js';
+
+const OBJECT_COUNT = 10;
+
+let AdvertisementList = createAdvertisementList(OBJECT_COUNT);
+console.log(AdvertisementList)
